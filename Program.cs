983edd19795359c293d6d0c393ea532290e151eb
Matheus <mthneri@gmail.com>
2020-11-17 20:27:42 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Do_While_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Deseja ver a tabuada de qual número?");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int resultado;
+
+
+            for(int contador = 10; contador >=1; contador--)
+            { 
+                // Código a se repetir
+                
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+                System.Threading.Thread.Sleep(1000);
+            }
+           }
+            
+            
+
+
+            
+           
+        }
+    }
