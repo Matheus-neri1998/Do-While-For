bcb5e25898a7071f9a52213831729a6c8da389f9
Matheus <mthneri@gmail.com>
2020-11-18 14:24:46 -0300

    Mudanças pontuais

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ namespace Do_While_For
 
             Console.WriteLine("Deseja ver a tabuada de qual número?");
             int resposta = int.Parse(Console.ReadLine());
-            System.Threading.Thread.Sleep(700);
 
-           for(int rp = 0; rp <=10; rp++)
+           for(int rp = 1; rp <=10; rp++)
             { 
                 Console.WriteLine($"{resposta} * {rp} = {resposta * rp}");
+                System.Threading.Thread.Sleep(1000);
                 
                 // $ = Interpolação 
                 // rp de resposta
